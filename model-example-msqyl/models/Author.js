06b0c8fe84f3bcf1f2d10d476f204fc0e573ccc4
alepereira <naitwa-alexandre@gmail.com>
2021-12-13 08:21:20 -0300

    mysql to mongodb

--- model-example-msqyl/models/Author.js
+++ model-example-msqyl/models/Author.js
@@ -24,11 +24,18 @@ const serialize = ({ id, first_name, middle_name, last_name }) => {
 }
 
 const getAll = async () => {
-  const [authors] = await connection.execute(
-    'SELECT id, first_name, middle_name, last_name FROM authors'
-  );
-
-  return authors.map(serialize).map(getNewAuthor);
+  return connection()
+    .then((db) => db.collection('authors').find().toArray())
+    .then((authors) => {
+      authors.map(({ _id, firstName, middleName, lastName }) => {
+        getNewAuthor({
+          id, _id,
+          firstName,
+          middleName,
+          lastName
+        });
+      });
+    });
 }
 
 const getById = async (id) => {
