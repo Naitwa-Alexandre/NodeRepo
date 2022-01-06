470d44ac3722ae39fdf4a5735cdfc23b0a79e799
alepereira <naitwa-alexandre@gmail.com>
2022-01-06 07:44:29 -0300

    first try

--- app-with-sequelize02/config/config.js
+++ app-with-sequelize02/config/config.js
@@ -1,23 +1,25 @@
+require('dotenv').config();
+
 module.exports = {
-  "development": {
-    "username": process.env.DB_NAME,
-    "password": process.env.PASSWORD,
-    "database": process.env.DB,
-    "host": process.env.HOST,
-    "dialect": "mysql"
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
   },
-  "test": {
-    "username": process.env.DB_NAME,
-    "password": process.env.PASSWORD,
-    "database": process.env.DB,
-    "host": process.env.HOST,
-    "dialect": "mysql"
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
   },
-  "production": {
-    "username": process.env.DB_NAME,
-    "password": process.env.PASSWORD,
-    "database": process.env.DB,
-    "host": process.env.HOST,
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+};
